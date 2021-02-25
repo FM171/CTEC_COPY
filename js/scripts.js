a2505d1d92d54c18d810187219712881510bd16e
Faranak <p17185722@my365.dmu.ac.uk>
2021-02-25 16:57:33 +0000

    page nav upgraded to be responsive and title animation added on db page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,44 @@
-console.log("hello");
+menuToggler.addEventListener('click', ev => {
+  menu.classList.toggle('open');
+  menuToggler.textContent= menuToggler.textContent === "X" ? "≡" : "X";
+
+});
+
+
+"use strict";
+
+const slides= document.querySelectorAll('#slideDeck section');
+let currentSlide = 0;
+
+function setSlide(slide_number) {
+  let current = document.querySelector('#slideDeck section.current');
+  if(current){
+    current.classList.remove('current');
+  }
+  let mySlide = slides.item(slide_number);
+  if(mySlide) {
+    mySlide.classList.add('current');
+  }
+  test.textContent =`${slide_number + 1} of ${slides.length}`;
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+
+function prevSlide(ev) {
+  currentSlide--;
+  if(currentSlide < 0){
+    currentSlide += slides.length - 1;
+  }
+  setSlide(currentSlide);
+}
+
+function nextSlide(ev) {
+  currentSlide++;
+  if(currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  setSlide(currentSlide);
+}
+
+setSlide(currentSlide);
